42fb3ba8c47d62c117ed287a844312a03d51a6f5
Andy Fragen <andy@thefragens.com>
2021-03-12 14:26:32 -0800

    composer

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -25,24 +25,24 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => 'ee623206da684f5d6d83677ff1727dae6843ad6e',
+    'reference' => '2489239f1eed1522fdec7f1e929063d588c8025f',
     'name' => 'afragen/git-updater-gitea',
   ),
   'versions' => 
   array (
     'afragen/git-updater-gitea' => 
     array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => 'ee623206da684f5d6d83677ff1727dae6843ad6e',
+      'reference' => '2489239f1eed1522fdec7f1e929063d588c8025f',
     ),
   ),
 );
