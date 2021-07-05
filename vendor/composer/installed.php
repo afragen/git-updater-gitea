e3cf1a3b046d63960839d9ad725d51aab303b4a9
Andy Fragen <andy@thefragens.com>
2021-07-05 16:11:43 -0700

    add PHP 5.6 compatibility

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,24 +1,23 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c19061b03f896f88e1cdcc5be4161751b1307e42',
+        'name' => 'afragen/git-updater-gitea',
+        'dev' => false,
     ),
-    'reference' => '5e2ed96f32cb0f6328065490d0ac3ba02a82bb82',
-    'name' => 'afragen/git-updater-gitea',
-  ),
-  'versions' => 
-  array (
-    'afragen/git-updater-gitea' => 
-    array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '5e2ed96f32cb0f6328065490d0ac3ba02a82bb82',
+    'versions' => array(
+        'afragen/git-updater-gitea' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c19061b03f896f88e1cdcc5be4161751b1307e42',
+            'dev_requirement' => false,
+        ),
     ),
-  ),
 );
