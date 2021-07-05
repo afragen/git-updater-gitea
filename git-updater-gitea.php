6d029eb61cdd8f04e59bcb5010f0b083f930cf1b
Andy Fragen <andy@thefragens.com>
2021-07-05 16:39:36 -0700

    release

--- git-updater-gitea.php
+++ git-updater-gitea.php
@@ -13,7 +13,7 @@
  * Plugin Name:       Git Updater - Gitea
  * Plugin URI:        https://github.com/afragen/git-updater-gitea
  * Description:       Add Gitea hosted repositories to the Git Updater plugin.
- * Version:           1.0.1.1
+ * Version:           1.1.0
  * Author:            Andy Fragen
  * License:           MIT
  * Network:           true
