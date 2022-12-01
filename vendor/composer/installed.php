169823b6364b7880f55a30d63faec3cc5d7f46dc
Andy Fragen <andy@thefragens.com>
2022-11-30 16:08:18 -0800

    add icon, update GA, release

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => 'afragen/git-updater-gitea',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
+        'reference' => 'dc3d9b681bd407df3d2a91be5ed7a8dc056083c8',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '89608ac007a4847d08c945e72ff2f04fa52f7fd2',
-        'name' => 'afragen/git-updater-gitea',
         'dev' => false,
     ),
     'versions' => array(
         'afragen/git-updater-gitea' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
+            'reference' => 'dc3d9b681bd407df3d2a91be5ed7a8dc056083c8',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '89608ac007a4847d08c945e72ff2f04fa52f7fd2',
             'dev_requirement' => false,
         ),
     ),
