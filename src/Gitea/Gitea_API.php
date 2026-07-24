78688f0783bec48360fe7e5bbd5f14e4de881570
Andy Fragen <andy@thefragens.com>
2026-07-24 08:24:31 -0700

    add guard to parse_contents_response()

--- src/Gitea/Gitea_API.php
+++ src/Gitea/Gitea_API.php
@@ -401,6 +401,13 @@ class Gitea_API extends API implements API_Interface {
 	protected function parse_contents_response( $response ) {
 		$files = [];
 		$dirs  = [];
+		if ( is_scalar( $response ) ) {
+			return [
+				'files' => $files,
+				'dirs'  => $dirs,
+			];
+		}
+
 		foreach ( $response as $content ) {
 			$content = (object) $content;
 			if ( property_exists( $content, 'type' ) && 'file' === $content->type ) {
